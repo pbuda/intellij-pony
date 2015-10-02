dd160bc693cda953dac2f6082072d77d1d426968
Piotr Buda <pibuda@gmail.com>
2015-10-02 12:21:36 +0200

    Adds tests for lexer

---
+++ tests/me/piotrbuda/intellij/pony/parser/PonyLexerTest.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2015 Piotr Buda
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package me.piotrbuda.intellij.pony.parser;
+
+import com.intellij.testFramework.ParsingTestCase;
+
+public class PonyLexerTest extends ParsingTestCase {
+    public PonyLexerTest() {
+        super("parser", "pony", new PonyParserDefinition());
+    }
+
+    public void testSimpleClassDef() {
+        doTest(true);
+    }
+
+    @Override
+    protected String getTestDataPath() {
+        return "testData";
+    }
+
+    @Override
+    protected boolean skipSpaces() {
+        return false;
+    }
+
+    @Override
+    protected boolean includeRanges() {
+        return true;
+    }
+
+}
